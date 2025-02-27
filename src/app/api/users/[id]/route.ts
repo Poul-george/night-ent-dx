510be656632cd144d32cc72548f62d5a03955826
Poul-george <65060101+Poul-george@users.noreply.github.com>
2025-02-28 05:35:18 +0900

    user詳細api

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,32 @@
+import { prisma } from '@/client/db'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(
+  request: NextRequest,
+  params: { params: { id: string } }
+) {
+  try {
+    const { id } = await params.params;
+    const user = await prisma.user.findFirst({
+      where: {
+        id: Number(id),
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        storeId: true,
+        createdAt: true,
+      },
+    })
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 })
+    }
+
+    return NextResponse.json(user)
+  } catch (error) {
+    console.error('Error fetching user:', error)
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 })
+  }
+} 
