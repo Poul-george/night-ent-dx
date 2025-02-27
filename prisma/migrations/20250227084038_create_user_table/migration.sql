c5f86cc4427a566d11daff196af85e2d30326432
Poul-george <65060101+Poul-george@users.noreply.github.com>
2025-02-28 05:43:05 +0900

    userテーブル作成migrations

---
+++ prisma/migrations/20250227084038_create_user_table/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "store_id" INTEGER,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_store_id_idx" ON "users"("store_id");
