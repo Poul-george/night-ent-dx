dfcb5c296fb143de42a8c0ec9f6b9c0ccb47116e
Poul-george <65060101+Poul-george@users.noreply.github.com>
2025-02-28 08:35:04 +0900

    デプロイ時のエラー解消

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -3,10 +3,10 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET(
   request: NextRequest,
-  params: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
-    const { id } = await params.params;
+    const id = (await params).id;
     const user = await prisma.user.findFirst({
       where: {
         id: Number(id),
