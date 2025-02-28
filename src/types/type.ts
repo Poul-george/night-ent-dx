8474bffb606503cbf378562c994c6f1bd480b27c
Poul-george <65060101+Poul-george@users.noreply.github.com>
2025-02-28 22:07:01 +0900

    キャストの型定義

--- src/types/type.ts
+++ src/types/type.ts
@@ -12,3 +12,14 @@ export type Store = {
   address: string | null;
   createdAt: string;
 };
+
+export type Cast = {
+  id: number;
+  storeId: number;
+  name: string;
+  salarySystem: number; // 0: 月給, 1: 時給
+  monthlySalary: number | null;
+  hourlyWage: number | null;
+  backSetting: number; // 0: バック設定なし, 1: バック設定あり
+  createdAt: string;
+};
